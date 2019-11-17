638db614068d26fe03ba221de0910b754f75cc40
jay <hongil0513@gmail.com>
2019-11-17 17:49:26 +0900

    2019/11/17 leetcode 344.Reverse String

--- 344. Reverse String/Solution.java
+++ 344. Reverse String/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         
-        int front=0;  // 인덱스 값 나타낼 변수 설정
+        int front=0;  // 앞의인덱스 값 나타낼 변수 설정
         
         while(front<(s.length-1)-front){      //앞과 뒤에서부터 하나씩 인덱스를 줄여가며 배열 값을 치환해줌 
             char temp=s[(s.length-1)-front];  //앞 뒤 인덱스가 중간에 교차할때까지 반복함 
