94c0bbf060819aa29b3223348e9a0d0da3ef1b67
jay <hongil0513@gmail.com>
2019-11-17 17:47:39 +0900

    344. Reverse String

---
+++ 344. Reverse String/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int front=0;  // 인덱스 값 나타낼 변수 설정
+        
+        while(front<(s.length-1)-front){      //앞과 뒤에서부터 하나씩 인덱스를 줄여가며 배열 값을 치환해줌 
+            char temp=s[(s.length-1)-front];  //앞 뒤 인덱스가 중간에 교차할때까지 반복함 
+            s[(s.length-1)-front]=s[front];
+            s[front]=temp;
+            front++;
+        }
+    }
+}
